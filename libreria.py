5f55e13c4aae359b5508dd6ecb1ba72e17c0e56c
jaferdy <jaferdi5@gmail.com>
2023-05-18 19:17:18 +0200

    paso3

--- libreria.py
+++ libreria.py
@@ -3,3 +3,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+'''
+Visualizamos la variable Sexo
+'''
+survived_sex = train_df[train_df['Survived']==1]['Sex'].value_counts(sort=False)
+dead_sex = train_df[train_df['Survived']==0]['Sex'].value_counts(sort=False)
+sex_df = pd.DataFrame([survived_sex,dead_sex],index=['Survived','Dead'])
+sex_df.plot(kind='bar', stacked=True)
+plt.xlabel('Sex')
+plt.ylabel('Number of passengers')
+plt.title('Survival rate by sex')
+st.pyplot()
