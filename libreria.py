214622a4877453810e6618cae89aedac90f4b81c
CarreteroMunozJoseIvan <119271499+CarreteroMunozJoseIvan@users.noreply.github.com>
2023-05-18 18:33:12 +0200

    DV3

--- libreria.py
+++ libreria.py
@@ -47,3 +47,19 @@ Creamos el modelo
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
 model = RandomForestClassifier(n_estimators=100, random_state=0)
 model.fit(X_train, y_train)
+
+# Developer 3 - Parte 4
+
+'''
+Visualizamos la variable Age
+'''
+bins = [0, 10, 20, 30, 40, 50, 60, 70, 80]
+train_df['AgeGroup'] = pd.cut(train_df['Age'], bins)
+survived_age = train_df[train_df['Survived']==1]['AgeGroup'].value_counts(sort=False)
+dead_age = train_df[train_df['Survived']==0]['AgeGroup'].value_counts(sort=False)
+age_df = pd.DataFrame([survived_age,dead_age],index=['Survived','Dead'])
+age_df.plot(kind='bar', stacked=True)
+plt.xlabel('Age Group')
+plt.ylabel('Number of passengers')
+plt.title('Distribution of passengers by age and survival')
+st.pyplot()
