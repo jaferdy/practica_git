8bc580cf584c075387cbda22b73d82b8a3d35410
CarreteroMunozJoseIvan <119271499+CarreteroMunozJoseIvan@users.noreply.github.com>
2023-05-18 18:10:58 +0200

    Dv3

--- libreria.py
+++ libreria.py
@@ -39,3 +39,11 @@ variables=['Pclass', 'Age', 'Sex', 'SibSp', 'Parch', 'Fare', 'CabinBool', 'Embar
 'Embarked_S', 'Embarked_Q']
 X = train_df[variables]
 y = train_df['Survived']
+
+# Developer 3 - Parte 2
+'''
+Creamos el modelo
+'''
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
+model = RandomForestClassifier(n_estimators=100, random_state=0)
+model.fit(X_train, y_train)
